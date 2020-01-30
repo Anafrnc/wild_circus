b9e4c5e8663f035379c22e071c88cf71cee531fd
Anais Francois <anais44.francois@gmail.com>
2020-01-31 00:40:03 +0100

    commit

--- src/Controller/PicturesController.php
+++ src/Controller/PicturesController.php
@@ -2,13 +2,8 @@
 
 namespace App\Controller;
 
-use App\Entity\Picture;
-use App\Form\PictureType;
 use App\Repository\PictureRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
