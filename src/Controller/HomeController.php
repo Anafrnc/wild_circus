4b2edc209e3067db33472a6b6e1a3da95c599cc7
Anais Francois <anais44.francois@gmail.com>
2020-01-30 11:22:17 +0100

    vue presentation done

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PresentationRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,9 +15,12 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="index")
      */
-    public function index(): Response
+    public function index(PresentationRepository $presentationRepository): Response
     {
+        $text = $presentationRepository->findAll();
+
         return $this->render('home/index.html.twig', [
-        ]);
+            'texts' => $text,
+            ]);
     }
 }
