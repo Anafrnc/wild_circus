a94c9593a95613ddfbdaa6d5477bde89ceb6f9a7
Anais Francois <anais44.francois@gmail.com>
2020-01-30 17:43:28 +0100

    view for form image

--- src/Controller/PicturesController.php
+++ src/Controller/PicturesController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Picture;
+use App\Form\PictureType;
 use App\Repository\CategoryRepository;
 use App\Repository\PictureRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -21,6 +26,36 @@ class PicturesController extends AbstractController
         $image = $pictureRepository->findAll();
         return $this->render('pictures/index.html.twig', [
             'images' => $image,
+        ]);
+    }
+
+    public function addPicture(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        PictureRepository $pictureRepository
+    ): Response {
+        $image = $pictureRepository->findAll();
+
+        $picture = new Picture();
+
+        $pictureForm = $this
+            ->createForm(PictureType::class, $picture)
+            ->remove('source')
+            ->remove('update_at')
+            ->add('send', SubmitType::class)
+        ;
+        $pictureForm->handleRequest($request);
+
+        if ($pictureForm->isSubmitted() && $pictureForm->isValid()) {
+            $picture->setUpdateAt(new \DateTime());
+            //$picture->setCategory($category);
+            $entityManager->persist($picture);
+            $entityManager->flush();
+        }
+
+        return $this->render('pictures/form.html.twig', [
+            'images' => $image,
+            'pictureForm' => $pictureForm->createView(),
         ]);
     }
 }
